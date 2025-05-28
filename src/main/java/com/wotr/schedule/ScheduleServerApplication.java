e64a240e356fab1880b0524ef633ffd88b33e79f
wonnx <labora7@naver.com>
2025-05-28 16:35:45 +0900

    initial commit

---
+++ src/main/java/com/wotr/schedule/ScheduleServerApplication.java
@@ -0,0 +1,13 @@
+package com.wotr.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServerApplication.class, args);
+	}
+
+}
