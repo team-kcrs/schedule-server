1be4a757e278d75cbe382969078d2b74f69f98a4
wonnx <labora7@naver.com>
2025-06-11 23:35:42 +0900

    add application-test.yml

--- src/test/java/com/wotr/schedule/ScheduleServerApplicationTests.java
+++ src/test/java/com/wotr/schedule/ScheduleServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.wotr.schedule;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScheduleServerApplicationTests {
 
 	@Test
